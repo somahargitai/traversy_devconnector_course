8af3ae336b6211d427d09ea05547c652de22eac8
Soma Hargitai <hargitai.soma@gmail.com>
2019-08-27 20:46:47 +0200

    profile started

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-router.get('/test', (req,res) => res.json(
-    {
-      msg: "profile Works"
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route GET api/profile/test
+// @desc Tests profile route
+// @access Public
+router.get("/test", (req, res) =>
+  res.json({
+    msg: "profile Works"
+  })
+);
+
+// @route GET api/profile
+// @desc get current users profile
+// @access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this use';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+  }
+);
+
+
+
+// @route POST api/profile
+// @desc add user profile
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(req.body.handle) profileFields.handle = req.body.handle;
+    if(req.body.company) profileFields.company = req.body.company;
+    if(req.body.website) profileFields.website = req.body.website;
+    if(req.body.location) profileFields.location = req.body.location;
+    if(req.body.bio) profileFields.bio = req.body.bio;
+    if(req.body.status) profileFields.status = req.body.status;
+    if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+    // Skills - split into array
+    if(typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',');
     }
-  ));
+    // Social
+    profileFields.social = {};
+    if(req.body.youtube) profileFields.youtube = req.body.youtube;
+    if(req.body.facebook) profileFields.facebook = req.body.facebook;
+    if(req.body.instagram) profileFields.instagram = req.body.instagram;
+    if(req.body.twitter) profileFields.twitter = req.body.twitter;
+    if(req.body.linkedin) profileFields.linkedin = req.body.linkedin;
+
+  }
+);
 
 module.exports = router;
